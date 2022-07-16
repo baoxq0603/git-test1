44b1ed2eb0e01e67d320026103a82c015c823cdb
baoxq0603 <101027776+baoxq0603@users.noreply.github.com>
2022-07-16 21:22:08 +0800

    pull commit

--- src/main/java/com/bxq/git/Gittest.java
+++ src/main/java/com/bxq/git/Gittest.java
@@ -6,5 +6,6 @@ public class Gittest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
