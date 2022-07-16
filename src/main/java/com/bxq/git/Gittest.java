bd7bb80646be448baa6f94533bf4936d23e3d094
BXQ <b1037113157@163.com>
2022-07-16 20:36:52 +0800

    第二次

--- src/main/java/com/bxq/git/Gittest.java
+++ src/main/java/com/bxq/git/Gittest.java
@@ -3,5 +3,6 @@ package com.bxq.git;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
